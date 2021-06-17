8fa33a79e17885f8438c847da7532df94dfc669d
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-06-17 14:46:47 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=29,b=30,c;
+  cout<<"Print the sum of two numbers\n";
+  cout<<"-------------------------------\n";
+  c=a+b;
+  cout<<"The sum of "<<a<<" and "<<b<<" is: "<<c<<"\n";
+  return 0;
+}
