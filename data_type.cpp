6122a8b5518b06df03453ae22b80f8821a0bb080
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-06-17 15:13:56 +0530

    Add files via upload

---
+++ data_type.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+ 
+int main()
+{
+  int a=20;
+  char b='A';
+  float c=1.15;
+  bool d=false;
+  string greeting="hello";
+  cout<<"The value of a is :"<<a<<"\n";
+  cout<<"The value of b is :"<<b<<"\n";
+  cout<<"The value of c is :"<<c<<"\n";
+  cout<<"The value of d is :"<<d<<"\n";
+  cout<<"The value of greeting is :"<<greeting<<"\n";
+  cout<<"The size of int is:"<<sizeof(int)<<"\n";
+  return 0;
+}
