ecc8699728e4cd6f8953c879249782d7f8bc8b20
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-06-25 09:54:16 +0530

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=3;
+	
+	switch(a)
+	{
+		case 1:
+               cout<<"its sunday\n";
+               break;
+        case 2:
+               cout<<"its monday\n";
+               break;
+        case 3:
+               cout<<"its tuesday\n";
+               break;
+        case 4:
+               cout<<"its wednesday\n";
+               break;
+        default:cout<<"other day\n";
+	}
+  return 0;
+}
+
